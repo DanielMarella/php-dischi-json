59034b8c34149c1557728de16fd5b6cb7e7afe8e
DanielMarella <marella.daniel96@gmail.com>
2023-07-05 17:42:05 +0200

    add bootstrap, generate all card with v-for, add some classes

--- index.php
+++ index.php
@@ -10,19 +10,38 @@
         <!-- Vue -->
         <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
         <!-- My_CSS -->
         <link rel="stylesheet" href="styles/style.css">
         <title>PhpDischiJson</title>
     </head>
 
     <body>
-        <div id="app">
-        {{ message }}
-            <h1>ciao</h1>
-        </div>
 
+        <header class="head ">
 
+        </header>
+        <main>
+            <div id="app">
+                    
+                <div class="container pt-5 ">
+                    <div class="row">
+                        <div class="col-4"  v-for="(item, index) in items" :key="index">
+                            <div class="card w-75 mb-4 align-items-center">
+                                <img :src="item.poster" class="card-img-top w-50 mt-4" alt="...">
+                                <div class="card-body text-center">
+                                    <h5 class="card-title ">{{item.title}}</h5>
+                                    <p class="card-text">{{item.author}}</p>
+                                    <p class="card-text">{{item.year}}</p>
 
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
         <script src="./script/main.js"></script>
     </body>
 </html>
