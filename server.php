3268e673503b475a1ef76a90e5243d0f5474c8eb
DanielMarella <marella.daniel96@gmail.com>
2023-07-05 16:18:27 +0200

    decode and encode json

--- server.php
+++ server.php
@@ -1,7 +1,9 @@
-<?php 
+<?php
+    $stringRawData = file_get_contents('./database/dischi.json');
 
+    $dischi = json_decode($stringRawData);
+    header('Content-Type: application/json');
+    echo json_encode($dischi);
 
-
-
-
+    // var_dump($stringRawData, $todoList);
 ?>
